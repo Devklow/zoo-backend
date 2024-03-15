680b62af90456ddaa9b0e53e5dd635de876cdd95
joshua.labbe <joshua.labbe@etu.univ-amu.fr>
2024-03-15 11:18:46 +0100

    4th try

--- src/test/java/org/formation/labbe/zoo/zoobackend/technique/CagePersistanteTest.java
+++ src/test/java/org/formation/labbe/zoo/zoobackend/technique/CagePersistanteTest.java
@@ -1,5 +1,9 @@
 package org.formation.labbe.zoo.zoobackend.technique;
 
+import org.formation.labbe.zoo.modele.Animal;
+import org.formation.labbe.zoo.modele.Lion;
+import org.formation.labbe.zoo.modele.technique.CagePleineException;
+import org.formation.labbe.zoo.modele.technique.PorteException;
 import org.formation.labbe.zoo.zoobackend.services.CageService;
 import org.formation.labbe.zoo.zoobackend.services.CageServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -69,5 +73,36 @@ class CagePersistanteTest {
     void getIdAnimal() {
         CagePersistante c = new CagePersistante(2, service);
         assertEquals(2, c.getIdAnimal());
+    }
+
+    @Test
+    void nourrir() {
+        CagePersistante c = new CagePersistante(2, service);
+        Lion l = new Lion();
+        c.ouvrir();
+        Animal old;
+        try {
+            old = c.sortir();
+            c.entrer(l, 99);
+        } catch (PorteException e) {
+            throw new RuntimeException(e);
+        } catch (CagePleineException e) {
+            throw new RuntimeException(e);
+        }
+        c.fermer();
+        assertEquals("je suis un(e) Lion je m'appelle Leo j'ai 34 an(s) et je pèse 156.0 kg", c.getCageInfo().pancarte());
+        c.nourrir();
+        assertEquals("je suis un(e) Lion je m'appelle Leo j'ai 34 an(s) et je pèse 157.9 kg", c.getCageInfo().pancarte());
+        c.ouvrir();
+        try {
+            c.sortir();
+            c.entrer(old, 2);
+            c.fermer();
+        } catch (PorteException e) {
+            throw new RuntimeException(e);
+        } catch (CagePleineException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
